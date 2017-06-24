d1b604f904b3b451e4f0e467b15ea4e946869fb7
wendenburg <kgde@wendenburg.de>
2017-06-24 18:28:52 +0200

    comments and naming

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,8 +23,8 @@ class Home extends Component {
     render() {
         return (
             <div className='homeWrapper'>
-                <h2>Testing electron and webpak</h2>
-                <p>Develop your Electron-App with Electron, Webpack, React and Sass</p>
+                <h2>Boilerplate for a cool Electron App</h2>
+                <p>Develop your Electron-app with Electron, Webpack 2, React and Sass</p>
                 <p>
                     If you want to communicate with electrons main process
                     you can create a condition based on the environment to
